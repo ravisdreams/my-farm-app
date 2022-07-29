42bd54657e2d0644e5e1bbeeca509474f599f32e
ravisdreams <ravis.dreams@hotmail.com>
2022-07-29 17:03:06 +0530

    registered cors

--- index.py
+++ index.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from routes.student import student_router
+from fastapi.middleware.cors import CORSMiddleware
+
+client_apps = ['http://localhost:3000']
 
 app = FastAPI()
 
 # Register router
 app.include_router(student_router)
+
+# add middleware to allow requests from other origins/sources
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=client_apps,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
